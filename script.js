e33917c89e36862a387dbc1129e9d65e794095f8
precious <precious.maina@student.moringaschool.com>
2026-04-20 09:11:45 +0300

    removed last line

--- script.js
+++ script.js
@@ -143,5 +143,3 @@ if (typeof module !== "undefined") {
         addListing
     };
 }
-
-This project is about Finding Safe, Verified and Affordable Homes Easily.it aims to solve Fake listings,Unclear rent prices,Time wasted on bad houses,No easy comparison;by enabling Search by budget,Verified listings ,Compare houses,Reviews from tenants
